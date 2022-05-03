373e660903a5bc7b12d5120b69791b2e72ba7536
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-05-03 23:18:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
 
 function App() {
+  const { darkMode } = useContext(DarkModeContext);
 
   return (
-    <div className="App">
+    <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
@@ -20,7 +24,7 @@ function App() {
               <Route path=":userId" element={<Single />} />
               <Route
                 path="new"
-                element={<New />}
+                element={<New inputs={userInputs} title="Add New User" />}
               />
             </Route>
             <Route path="products">
@@ -28,7 +32,7 @@ function App() {
               <Route path=":productId" element={<Single />} />
               <Route
                 path="new"
-                element={<New />}
+                element={<New inputs={productInputs} title="Add New Product" />}
               />
             </Route>
           </Route>
